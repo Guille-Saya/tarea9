e6a5c0192db51ac53ca39d00c66e88aa6253353c
guillesaya <guillermo.sayavedra@gmail.com>
2022-09-11 20:04:36 -0300

    tarea 9

---
+++ script.js
@@ -0,0 +1,13 @@
+/* primera parte tarea 9 */
+alert("Hello world!");
+/* segunda parte tarea 9 */
+var nombre = "Guillermo";
+var apellido = "Sayavedra";
+var antiguedad = "1971";
+var nombre_y_apellido = nombre + " " + apellido;
+
+nombre = "Aguante";
+apellido = "Boquita!!";
+
+console.log (nombre, apellido, antiguedad);
+alert(nombre_y_apellido)
